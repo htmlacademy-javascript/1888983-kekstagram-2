3e28c8ad3865b42d2f23918fb0c48193b2f6ed6e
Keks <keks@htmlacademy.ru>
2024-09-27 10:31:07 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/filter.js
+++ 12/js/filter.js
@@ -3,41 +3,39 @@ import {debounce} from './utils.js';
 import {thumbnailListElement} from './utils.js';
 
 const RENDER_DELAY = 500;
+const MAX_RANDOM_PHOTOS = 10;
 
 const filtersListElement = document.querySelector('.img-filters');
-const filterButtonElement = filtersListElement.querySelectorAll('.img-filters__button');
 
 const getFilteredPhotos = (filterId, photos) => {
-  const filteredPhotos = photos.slice();
   switch (filterId) {
     case 'filter-default':
-      return filteredPhotos;
+      return photos;
     case 'filter-random':
-      return filteredPhotos.sort(() => Math.random() - 0.5).slice(0, 10);
+      return [...photos].sort(() => Math.random() - 0.5).slice(0, MAX_RANDOM_PHOTOS);
     case 'filter-discussed':
-      return filteredPhotos.sort((photoA , photoB) => photoB.comments.length - photoA.comments.length);
+      return [...photos].sort((photoA , photoB) => photoB.comments.length - photoA.comments.length);
     default:
-      return filteredPhotos;
+      return photos;
   }
 };
 
 const renderFilteredPhotos = (id, photos) => {
-  const newGallery = getFilteredPhotos(id, photos);
+  const FilteredPhotos = getFilteredPhotos(id, photos);
   const pictures = thumbnailListElement.querySelectorAll('.picture');
-  for(let i = 0; i < pictures.length; i++) {
-    pictures[i].remove();
-  }
-  createThumbnails(newGallery, thumbnailListElement);
+  pictures.forEach((picture) => picture.remove());
+  createThumbnails(FilteredPhotos, thumbnailListElement);
 };
+
+const debouncedRenderFilteredPhotos = debounce((filterId, photos) => renderFilteredPhotos(filterId, photos), RENDER_DELAY);
 
 const setFilters = (photos) => {
   filtersListElement.addEventListener('click', (evt) => {
     if (evt.target.matches('.img-filters__button')) {
-      for(let i = 0; i < filterButtonElement.length; i++) {
-        filterButtonElement[i].classList.remove('img-filters__button--active');
-      }
+      const activeFilterButtonElement = filtersListElement.querySelector('.img-filters__button--active');
+      activeFilterButtonElement.classList.remove('img-filters__button--active');
       evt.target.classList.add('img-filters__button--active');
-      debounce(() => renderFilteredPhotos(evt.target.id, photos), RENDER_DELAY)();
+      debouncedRenderFilteredPhotos(evt.target.id, photos);
     }
   });
 };
